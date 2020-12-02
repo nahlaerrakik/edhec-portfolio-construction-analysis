018c976f0a11968614b3b1f5f6c478fe2d3a60ad
github nana <nahlaerrakik@users.noreply.github.com>
2020-12-03 00:41:55 +0100

    add data

---
+++ data/notes.py
@@ -0,0 +1,10 @@
+__author__ = 'nahla.errakik'
+
+
+"""Market capitalisation is the number of shares outstanding multiplied by the price ( the total value of a public company on the market at a given period of time."""
+
+"""Limits of Diversification:
+    Diversification is limited to market conditions, therefore it cannot help manage systematic risk. That's when hedging comes to the picture.
+    Hedging is effective at managing systematic risk but you have to  give up on the upside as well. Insurance is a type of dynamic hedging that can
+    offer the best of both worlds.
+"""
